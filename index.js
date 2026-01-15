6cb619bfbf602d29e9e47671f8613a50535ceaad
MrinmoyAdhikary <mrinmoy.adh@gmail.com>
2026-01-15 10:43:25 +0530

    Basic Structure of the Project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const path = require("path");
+const methodOverride = require('method-override')
 main().then(() => console.log("Connection success"))
 .catch(()=>console.log("fail"));
 
@@ -8,6 +10,13 @@ async function main(params) {
 }
 const app=express();
 const port=8080;
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"/public"))); 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(methodOverride('_method'))
+
 app.listen(port,()=>{
     console.log("App is listening");
 });
